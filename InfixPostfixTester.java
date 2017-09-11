406853ca95b33e2743f74c320ef09a934d9028ae
Audrey Meadows <s592714@sbuniv.edu>
2017-09-11 16:46:56 +0000

    First iteration

--- InfixPostfixTester.java
+++ InfixPostfixTester.java
@@ -4,28 +4,37 @@ Public Class InfixPostfixTester
     {
         Scanner scan = new Scanner(System.in);
         CharStack stack = new CharStack();
+        String output = "";
         
         System.out.print("Please enter infix notation: ");
         String input = scan.nextLine();
+        
         
         for (int i = 0; i< input.length; i++;)
         {
             if(input.charAt(i) == '('')
             {
                 //take it and put it in the stack
+                stack.push(charAt(i));
             }
+            
             else if (input.charAt(i) == ')')
             {
                 //pop operator out into output
-                //get rid of left paren in stack
+                output = output + " " +(stack.pop(charAt(i))).toString;
+                
+                //get rid of left paren in stack               
+                stack.pop();
             }
             else if (input.charAt(i) == '+' || input.charAt(i) == '-' || input.charAt(i) == '*' || input.charAt(i) == '/' || input.charAt(i) == '%')
             {
-                //put it in stack to wait until operand2 is printed
+                //put it in stack to wait until operand2 is printed <--THEN WHAT???
+                push(charAt(i));
             }
             else //number
             {
                 //write number to output
+                output = output + " " + charAt(i).toString;
             }
         }
     }//main
